725a7e276f4af11e8e69e0fce0540e4db74593cd
Mild-TN <mild20091931@gmail.com>
2018-11-06 00:08:17 +0700

    [UPDATE] แก้ __construct() ของ UserController.php

--- api/app/Http/Controllers/UserController.php
+++ api/app/Http/Controllers/UserController.php
@@ -7,11 +7,19 @@ use App\Model\User;
 
 class UserController extends Controller
 {
-    public function getUsers() {
-        return User::all();
+    protected $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepo){
+        $this->userRepository = $userRepo;
     }
 
-    public function getUserId($id) {
-        return User::where('id' , $id)->first();
+    public function getUsers(){
+        $users = $this->userRepository->userRooms();
+        return response()->json($users);
+    }
+
+    public function getUserById($id){
+        $userById = $this->userRepository->getUserById($id);
+        return response()->json($userById);
     }
 }
