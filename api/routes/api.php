594dfc06546885fde350074800a6618ccdcac42a
Mild-TN <mild20091931@gmail.com>
2018-11-16 01:33:09 +0700

    [UPDATE] header('Access-Control-Allow-Origin: *')

--- api/routes/api.php
+++ api/routes/api.php
@@ -12,6 +12,9 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -27,7 +30,6 @@ Route::get('room/{id}','RoomController@getRoomById');
 // Route::get('room/{room_name}','RoomController@createRoomCode');
 // Route::get('room/{room_code}','RoomController@getRoomByCode');
 // Route::post('room/{room_name}','RoomController@checkCode');
-Route::post('room','RoomController@createRoom');
 Route::post('room','RoomController@createRoom');
 Route::get('users', 'UserController@getUsers');
 Route::get('user/{id}', 'UserController@getUsersById');
